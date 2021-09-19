97e730b33697cb92ab5be48ef842508fc5e0e911
Mohamed Amine Belabbes <m.amine.belabbes@gmail.com>
2021-09-18 23:36:30 -0500

    Done

--- cv/blur_video.py
+++ cv/blur_video.py
@@ -15,13 +15,9 @@ def to_blur(target_face):
         target_face_img = Image.fromarray(target_face)
         start = time.time()
         target_embedding = img2vec.get_vec(target_face_img, tensor=True)
-        diff = time.time() - start
         for face_embedding in faces:
-            start = time.time()
             validation_score = metrics.pairwise.cosine_similarity(target_embedding.reshape((1, -1)), face_embedding.reshape((1, -1)))
-            diff = time.time() - start
-            #print(diff)
-            if validation_score > 0.5:
+            if validation_score > 0.8:
                 return True
     except Exception as e:
         print("To blur exception: ", e)
@@ -29,13 +25,23 @@ def to_blur(target_face):
 
 def read_video():
 
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture("demo.m4v")
+    print(cap.isOpened())
     frames = []
+    # while True:
+    #     success, frame = cap.read()
+    #     cv2.imshow('Pinnacle', frame)
+    #     if cv2.waitKey(5) & 0xFF == 27:
+    #         break
+    # cap.release()
+    # cv2.destroyAllWindows()
     with mp_face_detection.FaceDetection(
         model_selection=1, min_detection_confidence=0.5) as face_detection:
 
         while cap.isOpened():
             success, frame = cap.read()
+            if type(frame) == type(None):
+                break
             height_img, width_img, _ = frame.shape
 
             if not success:
@@ -61,19 +67,49 @@ def read_video():
                     cv2.rectangle(frame,(x,y),(x+width,y+height),(255,255,0),2)
                     blur_face = to_blur(face)
                     if blur_face:
-                        face_to_blur = (x, y, width, height)
-                    
-                    temp = cv2.resize(face, (5, 5), interpolation=cv2.INTER_LINEAR)
-                    output = cv2.resize(temp, (width, height), interpolation=cv2.INTER_NEAREST)
-                    frame[y:y+height, x:x+width] = output
-            
+                        temp = cv2.resize(face, (5, 5), interpolation=cv2.INTER_LINEAR)
+                        output = cv2.resize(temp, (width, height), interpolation=cv2.INTER_NEAREST)
+                        frame[y:y+height, x:x+width] = output
+            #cv2.imshow('Frame',frame)
+            if cv2.waitKey(5) & 0xFF == 27:
+                break
             frames.append(frame)
+            print("Frame added")
+            #print(frames)
+        print("DONE")
+        cap.release()
+        cv2.destroyAllWindows()
         fourcc = cv2.VideoWriter_fourcc('a','v','c','1')
-        writer = cv2.VideoWriter('blurred.mp4',fourcc, 25.0, (frame.shape[1], frame.shape[0]))
+        writer = cv2.VideoWriter('blurred_no_janice.mp4',fourcc, 25.0, (frames[0].shape[1], frames[0].shape[0]))
         
         for i in range(0, len(frames)):
             writer.write(frames[i])
         writer.release()
     
+def main():
+    james = cv2.cvtColor(cv2.imread("James.jpg"), cv2.COLOR_BGR2RGB)
+    janice = cv2.cvtColor(cv2.imread("Janice.jpg"), cv2.COLOR_BGR2RGB)
+    tele = cv2.cvtColor(cv2.imread("Tele.jpg"), cv2.COLOR_BGR2RGB)
+    friends = [james, tele]
+    for friend in friends:
+        height_img, width_img, _ = friend.shape
+        with mp_face_detection.FaceDetection(
+            model_selection=1, min_detection_confidence=0.5) as face_detection:
+                friend.flags.writeable = False
+                results = face_detection.process(friend)
 
-read_video()
+                friend.flags.writeable = True
+                friend = cv2.cvtColor(friend, cv2.COLOR_RGB2BGR)
+                if results.detections:
+                    for detection in results.detections:
+                        x = int(detection.location_data.relative_bounding_box.xmin * width_img)
+                        y = int(detection.location_data.relative_bounding_box.ymin * height_img)
+                        width = int(detection.location_data.relative_bounding_box.width * width_img)
+                        height = int(detection.location_data.relative_bounding_box.height * height_img)
+                        face = friend[y:y+height, x:x+width]
+                        face_img = Image.fromarray(face)
+                        embedding = img2vec.get_vec(face_img, tensor=True)
+                        faces.append(embedding)
+
+    read_video()
+main()
--- cv/stream.py
+++ cv/stream.py
@@ -1,90 +1,61 @@
+from logging import raiseExceptions
 import cv2
 import mediapipe as mp
 from sklearn import metrics
 from PIL import Image
 from img2vec_pytorch import Img2Vec
-import threading, queue, time
 import numpy as np
+from flask import Flask, Response
 
 mp_face_detection = mp.solutions.face_detection
 mp_drawing = mp.solutions.drawing_utils
 faces = []
 img2vec = Img2Vec(cuda=False)
+app = Flask(__name__)
 
-# def to_blur(target_face):
-#     try:
-#         target_face_img = Image.fromarray(target_face)
-#         start = time.time()
-#         target_embedding = img2vec.get_vec(target_face_img, tensor=True)
-#         diff = time.time() - start
-#         print(diff)
-#         for face_embedding in faces:
-#             start = time.time()
-#             validation_score = metrics.pairwise.cosine_similarity(target_embedding.reshape((1, -1)), face_embedding.reshape((1, -1)))
-#             diff = time.time() - start
-#             print(diff)
-#             if validation_score > 0.8:
-#                 return True
-#     except Exception as e:
-#         print("To blur exception: ", e)
-#     return False
+def get_iou(bounding_box1, bounding_box2):
+
+    x_left = max(bounding_box1['x1'], bounding_box2['x1'])
+    y_top = max(bounding_box1['y1'], bounding_box2['y1'])
+    x_right = min(bounding_box1['x2'], bounding_box2['x2'])
+    y_bottom = min(bounding_box1['y2'], bounding_box2['y2'])
+
+    if x_right < x_left or y_bottom < y_top:
+        return 0.0
+
+    intersection_area = (x_right - x_left) * (y_bottom - y_top)
+
+    bounding_box1_area = (bounding_box1['x2'] - bounding_box1['x1']) * (bounding_box1['y2'] - bounding_box1['y1'])
+    bounding_box2_area = (bounding_box2['x2'] - bounding_box2['x1']) * (bounding_box2['y2'] - bounding_box2['y1'])
+
+
+    iou = intersection_area / float(bounding_box1_area + bounding_box2_area - intersection_area)
+
+    return iou
 
 def to_blur(target_face):
     try:
         target_face_img = Image.fromarray(target_face)
-        start = time.time()
         target_embedding = img2vec.get_vec(target_face_img, tensor=True)
-        diff = time.time() - start
-        #print(diff)
         for face_embedding in faces:
-            start = time.time()
             validation_score = metrics.pairwise.cosine_similarity(target_embedding.reshape((1, -1)), face_embedding.reshape((1, -1)))
-            diff = time.time() - start
-            #print(diff)
-            print(validation_score)
-            if validation_score > 0.6:
+            if validation_score > 0.5:
                 return True
     except Exception as e:
         print("To blur exception: ", e)
     return False
 
-class Worker(threading.Thread):
-    def __init__(self, q, output_q):
-        self.q = q
-        self.output_q = output_q
-        super().__init__()
-    def run(self):
-        while True:
-            self.get_prediction(self.q.get())
-            self.q.task_done()
-            
-    def get_prediction(self, frame):
-        res = to_blur(frame)
-        if not res:
-            self.output_q.put_nowait(False)
-            return False
-        else:
-            self.output_q.put_nowait(res)
-            return True
-
 def get_stream():
 
-    threaded = 0
-    q = queue.Queue()
-    output_q = queue.Queue()
-    
-    t = Worker(q, output_q)
-    t.start()
-
-
-    cap = cv2.VideoCapture(0)
-    #cap = cv2.VideoCapture("rtmp://192.168.162.218/live/pinnacle")
+    #cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture("rtmp://192.168.162.218/live/pinnacle")
 
     counter = 0
-    modulo = 30
     
     with mp_face_detection.FaceDetection(
         model_selection=1, min_detection_confidence=0.5) as face_detection:
+
+        last_blured_face_bbox = None
         while True:
             success, frame = cap.read()
             height_img, width_img, _ = frame.shape
@@ -98,68 +69,132 @@ def get_stream():
 
             frame.flags.writeable = True
             frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-            if counter == 0:
-                last_detected_boxes = []
-                last_blurred_bool = []
+            
+            # iterate over all faces
+            bboxes = [ ]
+            face_to_blur = None
             if results.detections:
                 for detection in results.detections:
-                   
                     x = int(detection.location_data.relative_bounding_box.xmin * width_img)
                     y = int(detection.location_data.relative_bounding_box.ymin * height_img)
-                    
                     width = int(detection.location_data.relative_bounding_box.width * width_img)
                     height = int(detection.location_data.relative_bounding_box.height * height_img)
-                    current_center = np.array([(x+height)/2, (y+width)/2])[None]
-                    face = frame[y:y+height, x:x+width]
-                    blur_face = None
-                    if counter == 0:
-                        blur_face = to_blur(face)
-                        last_detected_boxes.append(current_center)
-                        last_blurred_bool.append(blur_face)
-                    if blur_face is None:
-                        idx = (((last_detected_boxes - current_center)**2).sum(axis=1)).argmin()
-                        print(len(last_blurred_bool), len(last_detected_boxes))
-                        print(idx)
-                        blur_face = last_blurred_bool[idx]
-                    if blur_face:
-                        print("Blur")
-                        w, h = (5, 5)
-                        if(type(face) == type(None)):
-                            pass
-                        else:
-                            try:
-                                temp = cv2.resize(face, (w, h), interpolation=cv2.INTER_LINEAR)
-                                output = cv2.resize(temp, (width, height), interpolation=cv2.INTER_NEAREST)
-                                frame[y:y+height, x:x+width] = output
-                            except Exception as e:
-                                print("Failed to blur: ", e)
-                if counter ==0:
-                    last_detected_boxes = np.concatenate(last_detected_boxes)
 
-                    # if not output_q.empty():
-                    #     blur = output_q.get()
+                    cv2.rectangle(frame,(x,y),(x+width,y+height),(255,255,0),2)
 
-                    #     if blur:
-                    #         w, h = (5, 5)
-                    #         if(type(face) == type(None)):
-                    #             pass
-                    #         else:
-                    #             try:
-                    #                 temp = cv2.resize(face, (w, h), interpolation=cv2.INTER_LINEAR)
-                    #                 output = cv2.resize(temp, (width, height), interpolation=cv2.INTER_NEAREST)
-                    #                 frame[y:y+height, x:x+width] = output
-                    #             except Exception as e:
-                    #                 print("Failed to blur: ", e)
+                    bboxes.append((x, y, width, height))
                     
-                    #     q.put_nowait(face)
-                    # elif not threaded:
-                    #     print("Here")
-                    #     threaded = 1
-                    #     q.put_nowait(face)
-                cv2.rectangle(frame,(x,y),(x+width,y+height),(0,255,0),2)
-                counter = (counter + 1) % modulo
+                    # if time to run embedding model
+                    if counter == 0:
+                        face = frame[y:y+height, x:x+width]
+                        blur_face = to_blur(face)
+                        if blur_face:
+                            face_to_blur = (x, y, width, height)
+
+            if counter != 0 and last_blured_face_bbox is not None:
+                closest_bbox = None
+                max_iou = 0
+                xl,yl,wl,hl = last_blured_face_bbox
+                last_bbox_as_dict = {"x1": xl, "y1": yl, "x2": xl+wl, "y2": yl+hl}
+                for (x,y,w,h) in bboxes:
+                    # compute IOU
+                    curr_bbox_as_dict = {"x1": x, "y1": y, "x2": x+w, "y2": y+h}
+                    curr_iou = get_iou(curr_bbox_as_dict, last_bbox_as_dict) 
+                    if curr_iou > max_iou:
+                        closest_bbox = (x,y,w,h)
+                        max_iou = curr_iou
+                print(f"best iou is {max_iou}")
+                face_to_blur = closest_bbox
+            elif counter !=0 and last_blured_face_bbox is None:
+                print("last time we ran embedding model, there was no match. cannot interpolate")
+
+            # blur face
+            if face_to_blur is not None:
+                x, y, width, height = face_to_blur
+                try:
+                    temp = cv2.resize(face, (5, 5), interpolation=cv2.INTER_LINEAR)
+                    output = cv2.resize(temp, (width, height), interpolation=cv2.INTER_NEAREST)
+                    frame[y:y+height, x:x+width] = output
+                except Exception as e:
+                    print("Exception: ", e)
+                if counter == 0:
+                    cv2.rectangle(frame,(x,y),(x+width,y+height),(0,255,0),2)
+                    print("ran embedding model and similar face detected")
+                else:
+                    cv2.rectangle(frame,(x,y),(x+width,y+height),(255,0,0),2)
+                    print("interpolated from preev embedding")
+            elif face_to_blur is None and counter == 0:
+                print("ran embedding model but no face is simlar")
+
+            if counter == 0:
+                last_blured_face_bbox = face_to_blur
+
+            counter = (counter + 1) % 10
+
+            # if counter == 0:
+            #     last_detected_boxes = []
+            #     last_blurred_bool = []
+            #     last_detected_bounding_boxes = []
+            # if results.detections:
+            #     temp_set = set()
+            #     for detection in results.detections:
+                   
+            #         x = int(detection.location_data.relative_bounding_box.xmin * width_img)
+            #         y = int(detection.location_data.relative_bounding_box.ymin * height_img)
+                    
+            #         width = int(detection.location_data.relative_bounding_box.width * width_img)
+            #         height = int(detection.location_data.relative_bounding_box.height * height_img)
+            #         current_center = np.array([(x+width)/2, (y+height)/2])[None]
+            #         face = frame[y:y+height, x:x+width]
+            #         blur_face = None
+            #         bounding_box_dict = {"x1": x, "y1": y, "x2": x+width, "y2": y+height}
+            #         if counter == 0:
+            #             blur_face = to_blur(face)
+            #             last_detected_boxes.append(current_center)
+            #             last_blurred_bool.append(blur_face)
+            #             last_detected_bounding_boxes.append(bounding_box_dict)
+            #         if blur_face is None:
+            #             ious = [get_iou(temp,bounding_box_dict) for temp in last_detected_bounding_boxes]
+            #             ious = np.array(ious)
+            #             print("IOUS", ious)
+            #             print(last_blurred_bool)
+            #             if ious.any():
+            #                 idx = (-ious).argsort()
+            #                 if idx in temp_set:
+            #                     raise RuntimeError
+            #                 temp_set.add(idx)
+                            
+            #                 print(idx)
+            #             else:
+            #                 idx = (((last_detected_boxes - current_center)**2).sum(axis=1)).argmin()
+            #             last_detected_boxes[idx] =(current_center)
+            #             last_detected_bounding_boxes[idx] =(bounding_box_dict)
+            #             print(len(last_blurred_bool), len(last_detected_boxes))
+            #             print(idx)
+            #             blur_face = last_blurred_bool[idx]
+            #         if blur_face:
+            #             print("Blur")
+            #             w, h = (5, 5)
+            #             if(type(face) == type(None)):
+            #                 pass
+            #             else:
+            #                 try:
+            #                     temp = cv2.resize(face, (w, h), interpolation=cv2.INTER_LINEAR)
+            #                     output = cv2.resize(temp, (width, height), interpolation=cv2.INTER_NEAREST)
+            #                     frame[y:y+height, x:x+width] = output
+            #                 except Exception as e:
+            #                     print("Failed to blur: ", e)
+            #     if counter ==0:
+            #         last_detected_boxes = np.concatenate(last_detected_boxes)
+
+            #    cv2.rectangle(frame,(x,y),(x+width,y+height),(0,255,0),2)
+                #counter = (counter + 1) % modulo
 
             cv2.imshow('Pinnacle', frame)
+            ret, buffer = cv2.imencode('.jpg', frame)
+            og_frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + og_frame + b'\r\n')
             if cv2.waitKey(5) & 0xFF == 27:
                 break
         cap.release()
@@ -186,7 +221,12 @@ def main():
                     face_img = Image.fromarray(face)
                     embedding = img2vec.get_vec(face_img, tensor=True)
                     faces.append(embedding)
+    print("We run here")
 
-    get_stream()
+@app.route('/video_feed')
+def video_feed():
+    return Response(get_stream(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-main()
+if __name__ == "__main__":
+    main()
+    app.run(debug=True)
